fe9508b4c20fd8835b85a6fa8b6692ac6999cab3
chethack <techbuddy022@gmail.com>
2023-10-29 00:24:34 +0530

    made a bsic header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,24 +3,22 @@ import logo from "../sita_logo.png";
 
 function Header() {
   return (
-    <nav className="w-screen flex justify-between items-center py-4 mx-auto bg-brand_brown">
-      <div className="flex flex-row justify-start items-center md:flex-[0.5] flex-initial">
+    <nav className="w-screen grid grid-cols-2 items-center py-2 mx-auto bg-brand_maroon">
+      <div className="flex items-center ml-8">
         <img
-          className="w-8 ml-8 cursor-pointer rounded-lg"
+          className="w-12 cursor-pointer rounded-lg mb-1"
           src={logo}
           alt="logo"
         />
-        <span className="text-2xl font-bold text-gray-800">SITA</span>
+        <span className="text-2xl heading text-brand_orange ml-4 mt-1">
+          SITA
+        </span>
       </div>
-      {
-        <ul className="md:flex md:flex-[0.5] text-black hidden list-none flex-row justify-between items-center flex-initial">
-          <li className="mx-4 cursor-pointer">Home</li>
-          <li className="mx-4 cursor-pointer">About</li>
-          <li className="mx-4 cursor-pointer">Contact</li>
-          <li className="mx-4 cursor-pointer">Login</li>
-          <li className="mx-4 cursor-pointer">Register</li>
-        </ul>
-      }
+      <ul className="flex justify-end mr-8 space-x-8 text-xl body text-brand_orange">
+        <li className="cursor-pointer justify-evenly">DashBoard</li>
+        <li className="cursor-pointer justify-evenly">Sustainability</li>
+        <li className="cursor-pointer justify-evenly">Sign Up</li>
+      </ul>
     </nav>
   );
 }
